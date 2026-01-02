1e05449568765877191dbc958bda15ac415875fa
mreglen <khramtsov.ilia.a@gmail.com>
2026-01-02 23:45:54 +0500

    228

--- backend/app/main.py
+++ backend/app/main.py
@@ -14,14 +14,7 @@ app.mount("/uploads", StaticFiles(directory="uploads"), name="uploads")
 
 app.add_middleware(
     CORSMiddleware, 
-    allow_origins=[
-        "http://localhost:3000",
-        "http://127.0.0.1:3000",
-        "http://192.168.0.101:3000",
-        "http://localhost:3001",
-        "http://127.0.0.1:3001",
-        "http://192.168.0.101:3001"
-    ],
+    allow_origins=['*'],
     allow_credentials=True,
     allow_methods=["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allow_headers=["*"],
