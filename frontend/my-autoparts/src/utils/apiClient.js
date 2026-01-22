c235c3b4addb0fc8bb76d87f8b145c94d0d7af18
mreglen <khramtsov.ilia.a@gmail.com>
2026-01-23 01:05:27 +0500

    проверка

--- frontend/my-autoparts/src/utils/apiClient.js
+++ frontend/my-autoparts/src/utils/apiClient.js
@@ -1,9 +1,8 @@
-// Centralized API client for consistent API calls
 import axios from 'axios';
 
 const API_BASE = process.env.REACT_APP_API_BASE_URL || 'http://195.24.65.251/api';
 const BACKEND_BASE = process.env.REACT_APP_BACKEND_BASE_URL || 'http://195.24.65.251';
-
+// локально
 // const API_BASE = 'http://127.0.0.1:8000/api';
 // const BACKEND_BASE = 'http://127.0.0.1:8000';
 
