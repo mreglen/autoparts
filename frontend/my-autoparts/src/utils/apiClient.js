0f0626ef761a19c8cb235e98a9d2ff1665c66da6
mreglen <khramtsov.ilia.a@gmail.com>
2026-01-28 00:46:18 +0500

    ip_update6

--- frontend/my-autoparts/src/utils/apiClient.js
+++ frontend/my-autoparts/src/utils/apiClient.js
@@ -1,8 +1,11 @@
 import axios from 'axios';
 
 
-const API_BASE = process.env.REACT_APP_API_BASE_URL || 'https://VM2512296768.vds.ru';
-const BACKEND_BASE = process.env.REACT_APP_BACKEND_BASE_URL || 'https://VM2512296768.vds.ru';
+const API_BASE = process.env.REACT_APP_API_BASE_URL || 'https://svoygarage.ru';
+const BACKEND_BASE = process.env.REACT_APP_BACKEND_BASE_URL || 'https://svoygarage.ru';
+
+// const API_BASE = process.env.REACT_APP_API_BASE_URL || 'https://VM2512296768.vds.ru';
+// const BACKEND_BASE = process.env.REACT_APP_BACKEND_BASE_URL || 'https://VM2512296768.vds.ru';
 
 // const API_BASE = process.env.REACT_APP_API_BASE_URL || 'https://195.24.65.251/api';
 // const BACKEND_BASE = process.env.REACT_APP_BACKEND_BASE_URL || 'https://195.24.65.251';
